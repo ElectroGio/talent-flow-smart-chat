e36d6f1743194a8e7f4f37e26310e24b70ab0977
Daniel F. Delgado <daniel.delgado@dupplex.co>
2025-08-28 14:04:56 -0500

    feat: added host

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: '165.227.58.188'
+  },
   plugins: [react()],
 })
